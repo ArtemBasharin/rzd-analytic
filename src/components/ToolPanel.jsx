795776ac6985633588fe971b403be8c15a55b975
ArtemBasharin <temon-bash@yandex.ru>
2023-10-04 10:26:55 +0300

    tool animation and naming updated

--- src/components/ToolPanel.jsx
+++ src/components/ToolPanel.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { setSourceState } from "../redux/filtersSlice";
 import axios from "axios";
@@ -9,10 +9,12 @@ import DropdownCurrentYear from "./DropdownCurrentYear";
 import DaysInGroupSetter from "./DaysInGroupSetter";
 import MinValueSetter from "./MinValueSetter";
 import PeriodSetter from "./PeriodSetter";
+import { CSSTransition, SwitchTransition } from "react-transition-group";
 
 const ToolPanel = () => {
   const toolPalette = useSelector((state) => state.filters.toolPalette);
   const dispatch = useDispatch();
+  const ref = useRef();
   let date = new Date();
 
   useEffect(() => {
@@ -32,15 +34,70 @@ const ToolPanel = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const timeout = 500;
   return (
     <div className="controls-container">
-      {toolPalette.unitsListVisibility && <DropdownUnits />}
-      {toolPalette.minValueVisibility && <MinValueSetter />}
-      {toolPalette.yearVisibility && <DropdownPastYear />}
-      {toolPalette.yearVisibility && <DropdownCurrentYear />}
-      {toolPalette.periodVisibility && <PeriodSetter />}
-      {toolPalette.daysInGroupVisibility && <DaysInGroupSetter />}
-      {toolPalette.datePickerVisibility && <DateRangePicker />}
+      <CSSTransition
+        in={toolPalette.unitsListVisibility}
+        timeout={timeout}
+        classNames="fade"
+        // nodeRef={ref}
+        unmountOnExit
+      >
+        <div>
+          <DropdownUnits />
+        </div>
+      </CSSTransition>
+      <CSSTransition
+        in={toolPalette.minValueVisibility}
+        timeout={timeout}
+        classNames="fade"
+        unmountOnExit
+      >
+        <MinValueSetter />
+      </CSSTransition>
+      <CSSTransition
+        in={toolPalette.yearVisibility}
+        timeout={timeout}
+        classNames="fade"
+        unmountOnExit
+      >
+        <DropdownPastYear />
+      </CSSTransition>
+      <CSSTransition
+        in={toolPalette.yearVisibility}
+        timeout={timeout}
+        classNames="fade"
+        unmountOnExit
+      >
+        <DropdownCurrentYear />
+      </CSSTransition>
+      <CSSTransition
+        in={toolPalette.periodVisibility}
+        timeout={timeout}
+        classNames="fade"
+        unmountOnExit
+      >
+        <PeriodSetter />
+      </CSSTransition>
+      <CSSTransition
+        in={toolPalette.daysInGroupVisibility}
+        timeout={timeout}
+        classNames="fade"
+        unmountOnExit
+      >
+        <DaysInGroupSetter />
+      </CSSTransition>
+      <CSSTransition
+        in={toolPalette.datePickerVisibility}
+        timeout={timeout}
+        classNames="fade"
+        unmountOnExit
+      >
+        <div className="datepicker_container">
+          <DateRangePicker />
+        </div>
+      </CSSTransition>
     </div>
   );
 };
